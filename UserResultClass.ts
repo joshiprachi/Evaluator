e6f95cb6ce4bb93c5b1acc39933ac8da4f926be2
joshiprachi <39512260+joshiprachi@users.noreply.github.com>
2018-07-11 10:32:19 +0100

    Add files via upload

---
+++ UserResultClass.ts
@@ -0,0 +1,11 @@
+export class UserResultClass {
+  email: string;
+  name: string;
+  tech: string;
+  category1: number;
+    category2: number;
+    category3: number;
+    finalResult: number;
+    org: string;
+    pos: string;
+}
